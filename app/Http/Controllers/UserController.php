c32a36f995d2abd04802de2662e486a9b0fd85a0
naimulislamshakil <ms0413354@gmail.com>
2026-06-07 16:15:57 +0600

    login user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-   function login( Request $req){
-    return $req->all();
-   }
+   function login(Request $req)
+{
+    $user = User::where("email", $req->email)->first();
+
+    if ($user && Hash::check($req->password, $user->password)) {
+        $req->session()->put('user', $user);
+        return redirect('/');
+    }
+
+    return "Username or password is not matched";
+}
 
    function showLogin(){
     return view('login');
--- database/migrations/2026_06_07_095249_create_users_table.php
+++ database/migrations/2026_06_07_095249_create_users_table.php
@@ -6,25 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            $table->string('name')->nullable(); // not mandatory
             $table->string('email')->unique();
             $table->string('password');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 };
